31d1abed56f8069eb016162874e1eb5eee330d3e
hosenur <hosenur.dev@gmail.com>
2025-08-02 07:10:52 +0530

    fix

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -12,5 +12,14 @@ export default defineNitroConfig({
         "Access-Control-Allow-Credentials": "true",
       },
     },
+    "/listener": {
+      cors: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": "true",
+      },
+    },
   },
 });
