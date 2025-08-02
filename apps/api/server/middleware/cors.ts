de05743d3d7ae7c7332b9f519934f7e3441f4f80
hosenur <hosenur.dev@gmail.com>
2025-08-02 09:51:51 +0530

    dynamic cors

--- apps/api/server/middleware/cors.ts
+++ apps/api/server/middleware/cors.ts
@@ -1,17 +1,22 @@
-
 // server/middleware/cors.ts
 export default defineEventHandler(async (event) => {
-  setHeaders(event, {
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With, Accept, Origin, X-CSRF-Token, X-Auth-Token, Cache-Control',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Max-Age': '86400', // Cache preflight for 24 hours
-    'Access-Control-Expose-Headers': 'Authorization, Content-Length, X-JSON'
-  })
+  const allowedOrigins = ["http://localhost:3000", "https://rahaman.email"];
 
-  if (getMethod(event) === 'OPTIONS') {
-    setResponseStatus(event, 204)
-    return ''
+  const origin = getHeader(event, "origin") as string;
+  console.log({origin})
+  const allowOrigin = allowedOrigins.includes(origin) ? origin : "false";
+
+  setHeaders(event, {
+    "Access-Control-Allow-Origin": allowOrigin,
+    "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    "Access-Control-Allow-Headers":
+      "Content-Type, Authorization, X-Requested-With, Accept, Origin, X-CSRF-Token, X-Auth-Token, Cache-Control",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Max-Age": "86400",
+    "Access-Control-Expose-Headers": "Authorization, Content-Length, X-JSON",
+  });
+
+  if (event.req.method === "OPTIONS") {
+    return (event.res.status = 204);
   }
-})
+});
