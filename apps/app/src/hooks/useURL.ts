c3003f35f42163a5d1301f5e37dfefbd5f1118ee
hosenur <hosenur.dev@gmail.com>
2025-08-02 06:39:07 +0530

    changes

--- apps/app/src/hooks/useURL.ts
+++ apps/app/src/hooks/useURL.ts
@@ -1,4 +1,4 @@
 export const useURL = (slug: string) => {
   console.log(import.meta.env.VITE_BACKEND_URL);
-  return `http"//${import.meta.env.VITE_BACKEND_URL}/${slug}`;
+  return `http://${import.meta.env.VITE_BACKEND_URL}/${slug}`;
 };
