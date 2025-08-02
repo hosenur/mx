35cf2277495bbe94b94d56473678b287df2ba6ea
hosenur <hosenur.dev@gmail.com>
2025-08-02 07:14:03 +0530

    cleanup

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -6,7 +6,7 @@ export default defineNitroConfig({
       cors: true,
       headers: {
         "Access-Control-Allow-Origin":
-          "https://mx-app-production.up.railway.app,https://rahaman.email,https://localhost:3000",
+          "https://rahaman.email",
         "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
         "Access-Control-Allow-Credentials": "true",
