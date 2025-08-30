49f266acc872c39dc03386dc00d968c4ecccb553
hosenur <hosenur.dev@gmail.com>
2025-08-30 06:51:08 +0530

    migrate

--- lib/mailgun.ts
+++
@@ -1,7 +0,0 @@
-import FormData from "form-data";
-import Mailgun from "mailgun.js";
-const mg = new Mailgun(FormData);
-export const mailgun = mg.client({
-  username: "api",
-  key: process.env.MAILGUN_API_KEY || "key-yourkeyhere",
-});
--- lib/r2.ts
+++
@@ -1,9 +0,0 @@
-import { S3Client } from "@aws-sdk/client-s3";
-export const r2 = new S3Client({
-  region: "auto",
-  endpoint: process.env.R2_ENDPOINT,
-  credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.R2_ACCESS_KEY_SECRET as string,
-  },
-});
