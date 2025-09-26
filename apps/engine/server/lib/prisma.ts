403c4474dbae460719313305ee0137649ffb6341
hosenur <hosenur.dev@gmail.com>
2025-09-27 03:50:28 +0530

    cleanup

--- apps/engine/server/lib/prisma.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "../../generated/prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-export const prisma = new PrismaClient().$extends(withAccelerate());
