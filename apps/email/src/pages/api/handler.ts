8e90b82ad45d6dde300acac9ae6d61dd06628bb8
hosenur <hosenur.dev@gmail.com>
2025-09-27 07:46:17 +0530

    added ai classification

--- apps/email/src/pages/api/handler.ts
+++ apps/email/src/pages/api/handler.ts
@@ -1,19 +1,32 @@
 // pages/api/inbound.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { InboundWebhookPayload } from "@inboundemail/sdk";
+import { generateObject } from "ai";
+import { createMistral } from "@ai-sdk/mistral";
+import { z } from "zod";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 
-  // ✅ Next.js parses JSON automatically if Content-Type: application/json
   const payload = req.body as InboundWebhookPayload;
+  const mistral = createMistral({
+    // custom settings
+  });
 
-  console.log("Received inbound email:", payload);
+  const { object } = await generateObject({
+    model: mistral("magistral-small-2507"),
+    schema: z.object({
+      recipe: z.object({
+        category: z.string(),
+      }),
+    }),
+    prompt: `Classify the email category : ${payload.email.subject}`,
+  });
 
   return res.status(200).json({ success: true });
 }
