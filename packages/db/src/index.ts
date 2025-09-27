47f321f826949141dc7d8c029c1566c084b94a45
hosenur <hosenur.dev@gmail.com>
2025-09-27 08:35:30 +0530

    fixes

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "./generated/prisma/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 export const prisma = new PrismaClient().$extends(withAccelerate());
